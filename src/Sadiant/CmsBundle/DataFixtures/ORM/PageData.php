aeff7f7b08ee0f7086dad370320c37fc38781f38
Vincent Guillon <vincentg@theodo.fr>
2011-06-20 11:45:00 +0200

    [develop] Page repository

---
+++ src/Sadiant/CmsBundle/DataFixtures/ORM/PageData.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Sadiant\CmsBundle\DataFixtures\ORM;
+
+use Sadiant\CmsBundle\Entity\Page;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+
+class PageData implements FixtureInterface
+{
+    public function load($manager)
+    {
+        // Create new page
+//        $page = new Page();
+//        $page->setId(1);
+//        $page->setName('Homepage');
+//        $page->setContent();
+//        $page->setSlug('homepage');
+//        $page->setBreadcrumb('Homepage');
+//        $page->setDescription();
+//        $page->setStatus();
+    }
+}
