4b4bceab63d4f696314d815895c1b2757c526945
Vincent Guillon <vincentg@theodo.fr>
2011-06-21 12:55:10 +0200

    [develop] Update tests folder

--- src/Sadiant/CmsBundle/Tests/Repository/PageRepositoryTest.php
+++ src/Sadiant/CmsBundle/Tests/Repository/PageRepositoryTest.php
@@ -11,7 +11,7 @@ use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\ORM\Query;
 
-class PageTest extends \PHPUnit_Framework_TestCase
+class PageRepositoryTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @var \Doctrine\ORM\EntityManager
@@ -59,58 +59,6 @@ class PageTest extends \PHPUnit_Framework_TestCase
 
         // Test number of status
         $this->assertEquals(4, count($availableStatus));
-    }
-
-    /**
-     * Test getParent function
-     *
-     * @author Vincent Guillon <vincentg@theodo.fr>
-     * @since 2011-06-20
-     */
-    public function testGetParent()
-    {
-        print_r("\n> Test \"getParent\" function");
-
-        // Retrieve entity manager
-        $em = $this->getEntityManager();
-
-        // Retrieve "about" page
-        $aboutPage = $em->getRepository('SadiantCmsBundle:Page')->findOneBy(array('slug' => 'about'));
-
-        // Test aboutPage
-        $this->assertInstanceOf('Sadiant\CmsBundle\Entity\Page', $aboutPage);
-        $this->assertEquals('About', $aboutPage->getName());
-        
-        // Retrieve parent page
-        $parentPage = $aboutPage->getParent();
-        $this->assertInstanceOf('Sadiant\CmsBundle\Entity\Page', $parentPage);
-        $this->assertEquals('Homepage', $parentPage->getName());
-    }
-
-    /**
-     * Test getChildren function
-     *
-     * @author Vincent Guillon <vincentg@theodo.fr>
-     * @since 2011-06-20
-     */
-    public function testGetChildren()
-    {
-        print_r("\n> Test \"getChildren\" function");
-
-        // Retrieve entity manager
-        $em = $this->getEntityManager();
-
-        // Retrieve "about" page
-        $homepage = $em->getRepository('SadiantCmsBundle:Page')->findOneBy(array('slug' => 'homepage'));
-
-        // Test hompepage
-        $this->assertInstanceOf('Sadiant\CmsBundle\Entity\Page', $homepage);
-        $this->assertEquals('Homepage', $homepage->getName());
-
-        // Retrieve children pages
-        $childrenPages = $homepage->getChildren();
-        $this->assertInstanceOf('Doctrine\ORM\PersistentCollection', $childrenPages);
-        $this->assertEquals(2, $childrenPages->count());
     }
 
     /**
