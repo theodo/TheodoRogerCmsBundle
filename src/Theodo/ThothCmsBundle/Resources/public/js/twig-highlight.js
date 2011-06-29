13858132028205abe22f466f46a2ee6aba5ede7d
Mathieu Dahne <mathieud@theodo.fr>
2011-06-29 17:34:34 +0200

    [develop] twighighlighter for layout and snippet

--- src/Theodo/ThothCmsBundle/Resources/public/js/twig-highlight.js
+++ src/Theodo/ThothCmsBundle/Resources/public/js/twig-highlight.js
@@ -1,5 +1,5 @@
 editAreaLoader.init({
-			id: "page_content"	// id of the textarea to transform		
+			id: textarea_id	// id of the textarea to transform		
 			,start_highlight: true	// if start with highlight
 			,allow_resize: "both"
 			,allow_toggle: true
