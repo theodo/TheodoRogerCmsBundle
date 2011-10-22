61281d15c673f31b4af293d9116cd236f595cbb9
Fabrice Bernhard <fabriceb@theodo.fr>
2011-10-22 17:55:39 +0200

    Remove CKEditor (WYSIWYG)

--- Resources/public/js/twig-highlight.js
+++ Resources/public/js/twig-highlight.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function ()
 {
-  /*
   editAreaLoader.init({
 			id: textarea_id	// id of the textarea to transform
 			,start_highlight: true	// if start with highlight
@@ -10,27 +9,4 @@ jQuery(document).ready(function ()
 			,language: "en"
 			,syntax: "twig"
 		});
-  */
-  CKEDITOR.config.enterMode = CKEDITOR.ENTER_BR;
-  CKEDITOR.config.height = '400px';
-  CKEDITOR.config.entities = false;
-  CKEDITOR.config.autoParagraph = false;
-  CKEDITOR.config.templates_replaceContent = false;
-  CKEDITOR.config.contentsCss = [
-      '/bundles/theodocitedelespace/css/frontend/base.css',
-      '/bundles/theodocitedelespace/css/frontend/common.css',
-  ];
-
-  textareas = jQuery('.textarea');
-  jQuery.each(textareas,
-    function(index, value) {
-        var editor = CKEDITOR.replace(
-          value,
-          {
-              stylesSet: 'citedelespace'
-          }
-        );
-        CKFinder.setupCKEditor( editor, '/bundles/theodorogercms/ckfinder/' ) ;
-    }
-  );
 });
