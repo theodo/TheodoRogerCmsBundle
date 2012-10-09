286aed6f1ac5418d777f4fd6d36bce40a0cf18e4
Marek Kalnik <marekk@theodo.fr>
2012-10-09 23:42:07 +0200

    Cleanup and composer.lock update

--- Tests/Form/Page/LayoutTypeTest.php
+++ Tests/Form/Page/LayoutTypeTest.php
@@ -18,44 +18,5 @@ class LayoutTypeTest extends TypeTestCase
         $form->bind($data);
         $this->assertTrue($form->isValid());
     }
-
-    /**
-array (
-  'name' => 'Theodo',
-  'slug' => 'theodo',
-  'breadcrumb' => 'Theodo',
-  'description' => 'Theodo page',
-  'title' => '',
-  'keywords' => '',
-  'contentType' => 'text/html',
-  'cacheable' => '1',
-  'lifetime' => '',
-  'content' => 
-  array (
-    'content' => 
-    array (
-      'content' => '<div id="theodo">
-
-    <h2>
-
-        Theodo</h2>
-
-</div>
-
-',
-      'footer' => 'Copyright Theodo 2011
-
-',
-    ),
-    'layout' => 
-    array (
-      'choice' => 'normal',
-      'text' => '',
-    ),
-  ),
-  '_token' => '5d2dabe9582589c269ae0dc4f56fd938d476eb05',
-  'parentId' => '1',
-)
-     */
 }
 
